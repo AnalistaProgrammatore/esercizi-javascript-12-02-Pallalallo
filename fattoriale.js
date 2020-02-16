1fb269b4855f556ce0afdec3183e366fea511c55
Pallalallo <59975252+Pallalallo@users.noreply.github.com>
2020-02-16 20:21:04 +0100

    Update fattoriale.js

--- fattoriale.js
+++ fattoriale.js
@@ -1,3 +1,8 @@
 /**
-* Scrivere qui il codice della funzione fattoriale(n)
+let x = 7;
+let FactRic = (x) => {
+    if (x < 2) return 1;
+    return FactRic(x - 1) * x;
+}
+console.log(FactRic(x));
 **/
